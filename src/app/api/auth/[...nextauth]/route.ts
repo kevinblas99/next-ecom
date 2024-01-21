2edff841d81ffd48f8d120cecb30c115cacfbdb3
kevinblas99 <116315967+kevinblas99@users.noreply.github.com>
2024-01-21 17:20:28 -0500

    route again

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,35 @@
-import authOptions from "./options";
+import { prisma } from "@/lib/db/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
 import NextAuth from "next-auth/next";
+import Google from "next-auth/providers/google";
+import { env } from "@/lib/env";
+import { mergeAnnonymousCartIntoUserCart } from "@/lib/db/cart";
+import { PrismaClient } from "@prisma/client";
+
+const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma as PrismaClient) as Adapter,
+    providers: [
+        Google({
+            clientId: env.GOOGLE_CLIENT_ID,
+            clientSecret: env.GOOGLE_CLIENT_SECRET,
+        })
+    ],
+    callbacks: {
+        session({session, user}) {
+            session.user.id = user.id
+        return session;            
+        },
+    },
+    events : {
+        async signIn({user}) {
+            await mergeAnnonymousCartIntoUserCart(user.id);
+        },
+    }
+}
+
+export default authOptions;
 
 const handler = NextAuth(authOptions);
 
