8dfb7f4f5b2b47a5e6567947c55b85af74c7abbe
kevinblas99 <116315967+kevinblas99@users.noreply.github.com>
2024-01-21 21:21:15 -0500

    fuck

--- src/lib/db/cart.ts
+++ src/lib/db/cart.ts
@@ -2,7 +2,7 @@ import { prisma } from "./prisma";
 import { cookies } from "next/dist/client/components/headers"
 import { Cart, CartItem, Prisma } from "@prisma/client";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOption";
 
 export type CartWithProducts = Prisma.CartGetPayload<{
     include: { items:{include: {product: true}}}
