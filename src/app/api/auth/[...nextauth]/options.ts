f9035bdf8b5b573324bfaaa801e4ec082454c847
kevinblas99 <116315967+kevinblas99@users.noreply.github.com>
2024-01-26 18:42:38 -0500

    changed nextauthoptions for authoptions

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,31 @@
+import { mergeAnnonymousCartIntoUserCart } from "@/lib/db/cart";
+import { prisma } from "@/lib/db/prisma";
+import { env } from "@/lib/env";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { AuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+
+  const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma as PrismaClient) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  events: {
+    async signIn({ user }) {
+      await mergeAnnonymousCartIntoUserCart(user.id);
+    },
+  },
+};
+
+export default authOptions;
